f40a325cf1154bec34db7607292df6b6a9fd114c
Patryk Krawczyk <patrick36212@gmail.com>
2021-09-05 16:56:16 +0200

    add calculate function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import Container from './Container';
 import Footer from './Footer';
 import Form from './Form';
-import "./index.css";
 import Section from './Section';
+import { currencies } from "./currencies";
+import { useState } from 'react';
 
 function App() {
+
+  const [result, setResult] = useState();
+
+  const calculateResult = (currency, amount) => {
+    const value = currencies.find(({ short }) => short === currency).value;
+
+    setResult({
+      sourceAmount: +amount,
+      targetAmount: amount / value,
+      currency,
+    });
+  };
+
   return (
     <Container>
-        <Section title="Exchange calculator" body={<Form />} />
+      <Section
+        title="Currency calculator"
+        body={
+        <Form
+          calculateResult={calculateResult}
+          result={result}
+        />}
+      />
       <Footer name="Patryk Krawczyk" year="2021" />
     </Container>
   );
