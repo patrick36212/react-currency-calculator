9474c70772a49ca1511388896bf8f74fd12444c4
Patryk Krawczyk <patrick36212@gmail.com>
2021-09-05 16:55:46 +0200

    add functions

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,33 +1,70 @@
 import "./style.css"
+import { currencies } from "../currencies";
+import { useState } from "react";
+import Result from "../Result";
 
-const Form = () => (
-    <form className="form">
-        <fieldset className="form__fieldset">
-            <p className="form__adnotation">Required fields mark *.</p>
-            <label className="form__label">
-                <span className="form__labelText">PLN Amount*: </span>
-                <input className="form__field" type="number" step="any" required />
-            </label>
-            <label className="form__label">
-                <span className="form__labelText">Exchange currency*: </span>
-                <select className="form__field" name="currency">
-                    <option value="Zero" selected>Choose currency</option>
-                    <option value="CHF">CHF</option>
-                    <option value="USD">USD</option>
-                    <option value="EUR">EUR</option>
-                    <option value="GBP">GBP</option>
-                </select>
-            </label>
-            <label className="form__label">
-                <span className="form__labelText">Exchange rate: </span>
-                <input className="form__field js-exchangeRate" type="number" value="0" readonly />
-            </label>
-            <div className="form__buttonContainer">
-                <button className="form__button ">Count</button>
-                <button className="form__button" type="reset">Clear</button>
-            </div>
-        </fieldset>
-    </form>
-);
+const Form = ({ calculateResult, result }) => {
+    const [amount, setAmount] = useState("");
+    const [currency, setCurrency] = useState(currencies[0].short);
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        calculateResult(amount, currency);
+    };
+
+    return (
+        <form className="form" onSubmit={onFormSubmit}>
+            <fieldset className="form__fieldset">
+                <p className="form__adnotation">Required fields mark *.</p>
+                <label className="form__label">
+                    <span className="form__labelText">PLN Amount*: </span>
+                    <input
+                        value={amount}
+                        onChange={(event) => setAmount(event.target.value)}
+                        className="form__field"
+                        type="number"
+                        step="0.01"
+                        required
+                        autoFocus
+                    />
+                </label>
+                <label className="form__label">
+                    <span className="form__labelText">Exchange currency*: </span>
+                    <select
+                        className="form__field"
+                        name="currency"
+                        value={currency}
+                        onChange={(event) => setCurrency(event.target.value)}
+                    >
+                        {currencies.map((currency) => (
+                            <option
+                                key={currency.short}
+                                value={currency.short}
+                            >
+                                {currency.name}
+                            </option>
+                        ))}
+                    </select>
+                </label>
+                <label className="form__label">
+                    <span className="form__labelText">Exchange currency rate: </span>
+                    <input
+                        className="form__field"
+                        type="number"
+                        readOnly
+                    />
+                </label>
+                <div className="form__buttonContainer">
+                    <button
+                        className="form__button"
+                    >
+                        Count
+                    </button>
+                </div>
+                <Result result={result} />
+            </fieldset>
+        </form>
+    );
+};
 
 export default Form;
