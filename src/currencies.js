db8bf5dd2db22305cb04fcd9036844f6392bae2e
Patryk Krawczyk <patrick36212@gmail.com>
2021-09-05 16:21:30 +0200

    currencies table

---
+++ src/currencies.js
@@ -0,0 +1,6 @@
+export const currencies = [
+    { name: "Euro", short: "EUR", value: 4.4278 },
+    { name: "Franc", short: "CHF", value: 4.2525 },
+    { name: "Dollar", short: "USD", value: 3.9012 },
+    { name: "Pound", short: "GBP", value: 5.3782 },
+];
