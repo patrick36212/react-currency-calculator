c49c4173e2fb397a44a940f9f500415da01990fc
Patryk Krawczyk <patrick36212@gmail.com>
2021-09-05 03:21:46 +0200

    Add Button component

---
+++ src/Button/index.js
@@ -0,0 +1,10 @@
+import "./style.css"
+
+const Button = () => (
+
+<div className="form__buttons">
+    <button className="button" type="reset">Wyczyść</button>
+</div>
+);
+
+export default Button;
