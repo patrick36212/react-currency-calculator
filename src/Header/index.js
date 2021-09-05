5d6776b36ea3c4bf29206d80713b60843a29fcf2
Patryk Krawczyk <patrick36212@gmail.com>
2021-09-05 04:16:31 +0200

    delete header

--- src/Header/index.js
+++
@@ -1,9 +0,0 @@
-import "./style.css"
-
-const Header = ({title}) => (
-    <header className="header">
-        <h1 className="header__title">{title}</h1>
-    </header>
-);
-
-export default Header;
