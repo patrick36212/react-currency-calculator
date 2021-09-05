f868a64d08007dd8a2dce8ee0ccd4db42efe4072
Patryk Krawczyk <patrick36212@gmail.com>
2021-09-05 02:59:35 +0200

    add footer component

---
+++ src/Footer/index.js
@@ -0,0 +1,9 @@
+import "./style.css"
+
+const Footer = ({ name, year }) => (
+    <footer className="footer">
+        <p className="footer__paragraph">Coded by {name} &copy; {year}</p>
+    </footer>
+);
+
+export default Footer;
