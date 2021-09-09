7806e29a05c3033492f39e3d9f8f54bafeb55991
Patryk Krawczyk <patrick36212@gmail.com>
2021-09-09 11:28:31 +0200

    controlled components

--- src/Form/index.js
+++ src/Form/index.js
@@ -2,16 +2,31 @@ import "./style.css"
 import { currencies } from "../currencies";
 import Result from "../Result";
 import Footer from "../Footer";
+import { useState } from "react/cjs/react.development";
 
 const Form = () => {
 
+    const [amount, setAmount] = useState("");
+    const [currency, setCurrency] = useState(currencies[0].short);
+    const [rate, setRate] = useState(currencies[0].value);
+
+    const toggleRate = () => {
+        setRate(currencies.find(({ short }) => short === currency).value);
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+    };
+
     return (
-        <form className="form">
+        <form className="form" onSubmit={onFormSubmit}>
             <fieldset className="form__fieldset">
                 <p className="form__adnotation">Required fields mark *.</p>
                 <label className="form__label">
                     <span className="form__labelText">PLN Amount*: </span>
                     <input
+                        value={amount}
+                        onChange={({ target }) => setAmount(target.value)}
                         className="form__field"
                         type="number"
                         step="0.01"
@@ -24,6 +39,8 @@ const Form = () => {
                     <select
                         className="form__field"
                         name="currency"
+                        value={currency}
+                        onChange={({ target }) => setCurrency(target.value)}
                     >
                         {currencies.map((currency) => (
                             <option
@@ -38,6 +55,8 @@ const Form = () => {
                 <label className="form__label">
                     <span className="form__labelText">Exchange currency rate: </span>
                     <input
+                        value={rate}
+                        onChange={({target}) => toggleRate(target.value)}
                         className="form__field"
                         type="number"
                         readOnly
@@ -51,7 +70,7 @@ const Form = () => {
                     </button>
                 </div>
                 <div className="result">
-                  Calculated result: <Result />
+                    Calculated result: <Result />
                 </div>
                 <Footer name="Patryk Krawczyk" year="2021" />
             </fieldset>
