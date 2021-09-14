51c5c7cc153294b46c241a10688e6948535fb761
Patryk Krawczyk <patrick36212@gmail.com>
2021-09-14 19:38:30 +0200

    component function change

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,16 +1,15 @@
 import "./style.css"
 
 const Result = ({ result }) => (
-    <p className="result__text">
-        {result !== undefined && (
-            <>
-                {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;={" "}
-                <strong>
-                    {result.targetResult.toFixed(2)}&nbsp;{result.currency}
-                </strong>
-            </>
-        )}
-    </p>
+
+    !!result && (
+        <p className="result__text">
+            {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;={" "}
+            <strong>
+                {result.targetResult.toFixed(2)}&nbsp;{result.currency}
+            </strong>
+        </p>
+    )
 );
 
 export default Result;
