bd539e6618c3425e7cec5329fc672f8e7e68731e
Patryk Krawczyk <patrick36212@gmail.com>
2021-09-14 19:17:08 +0200

    change array to object

--- src/currencies.js
+++ src/currencies.js
@@ -1,6 +1,6 @@
-export const currencies = [
-    { name: "Euro", short: "EUR", value: 4.4278 },
-    { name: "Franc", short: "CHF", value: 4.2525 },
-    { name: "Dollar", short: "USD", value: 3.9012 },
-    { name: "Pound", short: "GBP", value: 5.3782 },
-];
+export const currencies = {
+     EUR: 4.4278,
+     CHF: 4.2525,
+     USD: 3.9012,
+     GBP: 5.3782,
+};
