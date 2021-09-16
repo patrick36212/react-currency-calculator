2c5ee8183abe03c7da0d69ffcf42cf55c9b2e0ce
Patryk Krawczyk <patrick36212@gmail.com>
2021-09-16 12:20:31 +0200

    code format

--- src/App/Clock/index.js
+++ src/App/Clock/index.js
@@ -2,18 +2,18 @@ import { useState, useEffect } from "react";
 import "./style.css";
 
 const Clock = () => {
-
     const [date, setDate] = useState(new Date());
+    
     const dateFormat = (date) =>
-    date.toLocaleDateString("en-GB", {
-        weekday: "long",
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        day: "numeric",
-        month: "short",
-        year: "numeric",
-    });
+        date.toLocaleString("en-GB", {
+            weekday: "long",
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric",
+            day: "numeric",
+            month: "short",
+            year: "numeric",
+        });
 
     useEffect(() => {
         const intervalId = setInterval(() => {
