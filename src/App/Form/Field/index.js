121091fbc1f84e23dba308fe104e0c653f86632f
Patryk Krawczyk <patrick36212@gmail.com>
2021-10-11 15:01:29 +0200

    create reuseable Field Component

---
+++ src/App/Form/Field/index.js
@@ -0,0 +1,28 @@
+import { currencies } from "../../currencies";
+import "./style.css";
+
+const Field = ({ fieldName, name, value, onChange }) => {
+
+    return (
+        <label className="form__label">
+            <span className="form__labelText">{fieldName}</span>
+            <select
+                className="form__field"
+                name={name}
+                value={value}
+                onChange={onChange}
+            >
+                {Object.keys(currencies).map(currency => (
+                    <option
+                        key={currency}
+                        value={currency}
+                    >
+                        {currency}
+                    </option>
+                ))}
+            </select>
+        </label>
+    );
+};
+
+export default Field;
