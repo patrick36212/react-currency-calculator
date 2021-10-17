1b750616bc994498ce716cd6d2770f97e6ea3fba
Patryk Krawczyk <patrick36212@gmail.com>
2021-10-17 13:22:33 +0200

    change main component to styled component

---
+++ src/App/Container/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.main`
+    width: 100%;
+    max-width: 1048px;
+    padding: 8px;
+
+    @media (max-width: 767px) {
+        margin: auto;
+    }
+`;
