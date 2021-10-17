623e66f67833fa446102eea99463be182596d548
Patryk Krawczyk <patrick36212@gmail.com>
2021-10-17 14:33:02 +0200

    change Clock component to styled component

--- src/App/Clock/index.js
+++ src/App/Clock/index.js
@@ -1,34 +1,13 @@
-import { useState, useEffect } from "react";
-import "./style.css";
+import { StyledClock } from "./styled";
+import { dateFormat, useCurrentDate } from "./useLocalCurrentDate";
 
 const Clock = () => {
-    const [date, setDate] = useState(new Date());
-    
-    const dateFormat = (date) =>
-        date.toLocaleString("en-GB", {
-            weekday: "long",
-            hour: "numeric",
-            minute: "numeric",
-            second: "numeric",
-            day: "numeric",
-            month: "short",
-            year: "numeric",
-        });
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setDate(new Date());
-        }, 1000);
-
-        return () => {
-            clearInterval(intervalId);
-        };
-    }, []);
+    const date = useCurrentDate();
 
     return (
-        <p className="clock">
+        <StyledClock>
             {`${dateFormat(date)}`}
-        </p>
+        </StyledClock>
     );
 };
 
---
+++ src/App/Clock/styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledClock = styled.p`
+    padding: 5px;
+    font-weight: bold;
+    margin: 0;
+    text-align: right;
+`;
