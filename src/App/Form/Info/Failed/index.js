3726dc21be9ccf6bab9ebacffba172c752548866
Patryk Krawczyk <patrick36212@gmail.com>
2021-10-29 08:44:15 +0200

    add failed icon

---
+++ src/App/Form/Info/Failed/index.js
@@ -0,0 +1,12 @@
+import { StyledIcon } from "./styled";
+
+
+const Failed = () => {
+    return (
+        <StyledIcon className="material-icons">
+            report_problem
+        </StyledIcon>
+    )
+}
+
+export default Failed;
---
+++ src/App/Form/Info/Failed/styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledIcon = styled.span`
+    font-size: 50px;
+    color: ${({ theme }) => theme.color.red};
+`;
