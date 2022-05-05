c306ef00b2affecb5a819802259d9d45f09abfc5
Patryk Krawczyk <patrick36212@gmail.com>
2022-05-05 18:52:40 +0200

    delete theme from index

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
-import { GlobalStyle } from './GlobalStyle';
-import { ThemeProvider } from 'styled-components';
-import { light, dark } from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={light}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('calc')
 );
