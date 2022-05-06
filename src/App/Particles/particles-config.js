96f1e6e9739e95325d24728fe940e88be592b010
Patryk Krawczyk <patrick36212@gmail.com>
2022-05-06 21:44:15 +0200

    install and configure particles package

---
+++ src/App/Particles/particles-config.js
@@ -0,0 +1,60 @@
+const particlesConfig = {
+    background: {
+        color: {
+            value: "#410590",
+        },
+    },
+    fpsLimit: 60,
+    interactivity: {
+        events: {
+            onHover: {
+                enable: true,
+                mode: "grab",
+            },
+            resize: true,
+        },
+        modes: {
+            grab: {
+                distance: 200,
+                duration: 0.4,
+            },
+        },
+    },
+    particles: {
+        color: {
+            value: "#ffffff",
+        },
+        links: {
+            color: "#ffffff",
+            distance: 130,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+        },
+        move: {
+            direction: "none",
+            enable: true,
+            speed: 2,
+            straight: false,
+        },
+        number: {
+            density: {
+                enable: true,
+                area: 800,
+            },
+            value: 130,
+        },
+        opacity: {
+            value: 0.5,
+        },
+        shape: {
+            type: "circle",
+        },
+        size: {
+            value: { min: 1, max: 5 },
+        },
+    },
+    detectRetina: true,
+}
+
+export default particlesConfig;
---
+++ src/App/Particles/styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ParticlesContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #b61924;
+  background-image: url('');
+  background-size: cover;
+  background-position: 50% 50%;
+  background-repeat: no-repeat;
+`;
