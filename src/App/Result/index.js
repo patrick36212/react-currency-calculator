a346002e3ab09670ed93365846beab9d65a7dc42
Patryk Krawczyk <patrick36212@gmail.com>
2022-05-07 18:29:03 +0200

    add additional info in result

--- src/App/Result/index.js
+++ src/App/Result/index.js
@@ -1,14 +1,22 @@
-import { StyledResult } from "./styled";
+import { StyledResult, StyledResultWrapper } from "./styled";
 
 const Result = ({ result }) => (
 
     !!result && (
-        <StyledResult className="result__text">
-            {result.sourceAmount.toFixed(2)}&nbsp;{result.ownedCurrency}&nbsp;={" "}
-            <strong>
-                {result.targetResult.toFixed(2)}&nbsp;{result.targetCurrency}
-            </strong>
-        </StyledResult>
+        <StyledResultWrapper>
+            <StyledResult>
+                {result.sourceAmount.toFixed(2)}&nbsp;{result.ownedCurrency}&nbsp;={" "}
+                <strong>
+                    {result.targetResult.toFixed(2)}&nbsp;{result.targetCurrency}
+                </strong>
+            </StyledResult>
+            <StyledResult additionalInfo>
+                {result.ownedRate}&nbsp;{result.ownedCurrency}&nbsp;={" "}
+            </StyledResult>
+            <StyledResult additionalInfo>
+                {result.targetRate}&nbsp;{result.targetCurrency}
+            </StyledResult>
+        </StyledResultWrapper>
     )
 );
 
--- src/App/Result/styled.js
+++ src/App/Result/styled.js
@@ -1,7 +1,17 @@
 import styled from "styled-components";
+import { css } from "styled-components";
+
+export const StyledResultWrapper = styled.div`
+    width: 100%;
+    display: grid;
+    grid-gap: 3px;
+`;
 
 export const StyledResult = styled.p`
-    margin: 0;
-    padding: 10px;
+    margin: 2px;
     font-size: 20px;
+
+    ${({ additionalInfo }) => additionalInfo && css`
+        font-size: 12px;
+    `}
 `;
