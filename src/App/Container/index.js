385b610db176693a5b7aa455d29c255cbc89ce14
Patryk Krawczyk <patrick36212@gmail.com>
2022-05-07 18:27:59 +0200

    container component

---
+++ src/App/Container/index.js
@@ -0,0 +1,11 @@
+import { StyledContainer } from "./styled";
+
+const Container = ({ children }) => {
+    return (
+        <StyledContainer>
+            {children}
+        </StyledContainer>
+    )
+};
+
+export default Container;
---
+++ src/App/Container/styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const StyledContainer = styled.div`
+    position: relative;
+    overflow: hidden;
+    width: 100%;
+`;
