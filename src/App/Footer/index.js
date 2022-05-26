54586078c98dc367305d1bbad17610701b1c88b5
Patryk Krawczyk <patrick36212@gmail.com>
2022-05-26 17:53:03 +0200

    make new footer & add social icons

--- src/App/Footer/index.js
+++ src/App/Footer/index.js
@@ -1,8 +1,29 @@
-import { StyledFooter } from "./styled";
+import { StyledAbout, StyledLink, StyledFooter, StyledFooterHeader, StyledFooterWrapper, StyledSocial, StyledSocialList, GithubIcon, LinkedInIcon } from "./styled";
 
-const Footer = ({ name, year }) => (
+const Footer = () => (
     <StyledFooter>
-        <p>{`Coded by`} {name} &copy; {year}</p>
+        <StyledFooterWrapper>
+            <StyledAbout>
+                {`This project was created during the YouCode course. It covers all the techniques learned in this course. This project is made for practice only, not for profit. Creating web-pages and applications is my passion. If you want to check my work, below you can find more info.`}
+            </StyledAbout>
+            <StyledFooterHeader>Contact me if my work seems interesting to you!</StyledFooterHeader>
+            <StyledLink href="mailto:patrick36212@gmail.com">
+                patrick36212@gmail.com
+            </StyledLink>
+            <StyledSocialList>
+                <StyledSocial>
+                    <StyledLink href="https://github.com/patrick36212">
+                        <GithubIcon />
+                    </StyledLink>
+                </StyledSocial>
+                <StyledSocial>
+                    <StyledLink href="https://www.linkedin.com/in/patryk-krawczyk-b09413188">
+                        <LinkedInIcon />
+                    </StyledLink>
+                </StyledSocial>
+            </StyledSocialList>
+            <StyledAbout>{`Coded by Patryk Krawczyk`} &copy; {2022}</StyledAbout>
+        </StyledFooterWrapper>
     </StyledFooter>
 );
 
--- src/App/Footer/styled.js
+++ src/App/Footer/styled.js
@@ -1,8 +1,84 @@
 import styled from "styled-components";
+import { ReactComponent as Github } from "./github.svg";
+import { ReactComponent as LinkedIn } from "./linkedin.svg";
 
 export const StyledFooter = styled.footer`
-    text-align: center;
-    padding: 10px;
-    color: ${({ theme }) => theme.colors.colorPrimary};
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    margin-top: 50px;
+    color: ${({ theme }) => theme.colors.colorSeconadry};
     background: ${({ theme }) => theme.colors.backgroundAddPrimary};
+`;
+
+export const StyledFooterWrapper = styled.div`
+    max-width: 70%;
+    margin: auto;
+    padding: 12px 0;
+`;
+
+export const StyledFooterHeader = styled.h4`
+    font-size: 16px;
+    margin: 10 0px;
+    font-weight: normal;
+    overflow: hidden;
+    white-space: nowrap;
+    text-transform: uppercase;
+    animation: 
+        typing 8s infinite;
+
+    @keyframes  typing {
+        from {
+            width: 0%;
+            }
+        to {
+            width: 100%;
+            }
+        }
+`;
+
+
+export const StyledLink = styled.a`
+    font-size: 24px;
+    font-weight: bold;
+    text-decoration: none;
+    color: ${({ theme }) => theme.colors.colorSeconadry};
+    transition: color 1s;
+
+    &:hover{
+        color: ${({ theme }) => theme.colors.colorPrimary};
+    }
+`;
+
+export const StyledAbout = styled.p`
+    max-width: 60%;
+`;
+
+export const StyledSocialList = styled.ul`
+    display: flex;
+    align-items: flex-start;
+    list-style: none;
+    padding: 0;
+`;
+
+export const StyledSocial = styled.li`
+    margin-right: 16px;
+`;
+
+export const GithubIcon = styled(Github)`
+    fill: ${({ theme }) => theme.colors.colorSeconadry};
+    transition: 1s;
+
+    &:hover{
+        fill: ${({ theme }) => theme.colors.colorPrimary};
+    }
+`;
+
+export const LinkedInIcon = styled(LinkedIn)`
+    fill: ${({ theme }) => theme.colors.colorSeconadry};
+    transition: 1s;
+
+    &:hover{
+        fill: ${({ theme }) => theme.colors.colorPrimary};
+    }
 `;
