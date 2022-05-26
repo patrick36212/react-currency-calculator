f89cc7bb15a2e46aeb2593dd65c0e816e04c86a5
Patryk Krawczyk <patrick36212@gmail.com>
2022-05-26 17:51:46 +0200

    add transition to all page

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -8,6 +8,7 @@ export const GlobalStyle = createGlobalStyle`
             -webkit-font-smoothing: antialiased;
             -moz-osx-font-smoothing: grayscale;
             box-sizing: border-box; 
+            transition: 1s cubic-bezier(0.69, 0.15, 0.31, 0.85);
         }
 
         *, ::after, ::before {
