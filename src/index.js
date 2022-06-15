1d47ff120813830e1b44f729065dc0349386a555
Patryk Krawczyk <patrick36212@gmail.com>
2022-06-15 14:23:08 +0200

    change in index

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { Provider } from 'react-redux';
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from './GlobalStyle';
-
+import store from './store';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store} />
-    <ThemeProvider >
-      <GlobalStyle />
       <App />
-    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('calc')
 );
