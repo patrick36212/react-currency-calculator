bfee868da5d09e644a0824f128922b2447fab9b6
Patryk Krawczyk <patrick36212@gmail.com>
2022-06-15 14:25:38 +0200

    add theme provider, globalstyle and particles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from 'react';
-import Exchange from './Exchange/Exchange';
+import { ThemeProvider } from 'styled-components';
+import { dark, light } from './theme';
+import { particlesConfigLight, particlesConfigDark } from "./common/Particles/particles-config";
+import {GlobalStyle} from './GlobalStyle';
+import { useSelector } from "react-redux";
+import { selectIsDarkTheme } from "./common/Header/ThemeSwitch/themeSlice";
+import Exchange from './Exchange';
+import ParticlesBackground from './common/Particles';
 
-const App = () => <Exchange />;
+const App = () => {
+    const isDarkTheme = useSelector(selectIsDarkTheme);
+
+    return (
+        <ThemeProvider theme={isDarkTheme ? dark : light} >
+            <ParticlesBackground options={isDarkTheme ? particlesConfigLight : particlesConfigDark} />
+            <GlobalStyle />
+            <Exchange />
+        </ThemeProvider>
+    );
+};
 
 export default App;
