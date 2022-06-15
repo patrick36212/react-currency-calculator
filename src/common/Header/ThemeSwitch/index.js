50d239560efb59cab1f921f63e3e069506953035
Patryk Krawczyk <patrick36212@gmail.com>
2022-06-15 14:22:39 +0200

    move themeSwitch to redux

--- src/common/Header/ThemeSwitch/index.js
+++ src/common/Header/ThemeSwitch/index.js
@@ -1,17 +1,21 @@
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTheme, selectIsDarkTheme } from "./themeSlice";
 import { Button, Info, SwitchBody, IconBody, Icon } from "./styled";
 
-export const ThemeSwitch = ({ onClick, status, active }) => {
+export const ThemeSwitch = () => {
+  const isDarkTheme = useSelector(selectIsDarkTheme);
+  const dispatch = useDispatch();
 
   return (
       <Button
-        onClick={onClick}
+        onClick={() => dispatch(toggleTheme)}
       >
         <Info>
-          Dark mode {status}
+          Dark mode {isDarkTheme ? "on" : "off"}
         </Info>
         <SwitchBody>
           <IconBody
-            active={active}
+            active={isDarkTheme}
           >
             <Icon />
           </IconBody>
